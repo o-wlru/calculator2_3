3e092b0446f802aa79f8c34dc801d596353f48f9
owlru <valujenich@inbox.ru>
2022-02-02 02:02:43 +0300

    Первый коммит

---
+++ src/main/java/com/skypro/calculator2_3/Calculator23Application.java
@@ -0,0 +1,13 @@
+package com.skypro.calculator2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/calculator2_3/Calculator23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.calculator2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
