cc3b333810c26d3b56043459c40a0a9734a02dbe
owlru <valujenich@inbox.ru>
2022-02-02 02:10:26 +0300

    итоговая версия

---
+++ src/main/java/com/skypro/calculator2_3/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.skypro.calculator2_3;
+
+public interface CalculatorService {
+
+    public int plus(int num1, int num2);
+
+    public int minus(int num1, int num2);
+
+    public double divide(int num1, int num2);
+
+    public int multiply(int num1, int num2);
+
+}
